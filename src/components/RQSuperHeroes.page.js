8dbe25852e5ea863512c19aee0e8888dceff85ba
AnwarHossainSR <mahedianwar@gmail.com>
2022-01-01 20:48:16 +0600

    tradional way

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,40 +1,7 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 const RQSuperHeroPage = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:4000/superheroes")
-      .then((res) => {
-        setData(res.data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setError(error.message);
-        setIsLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <h2>Loading...</h2>;
-  }
-
-  if (error) {
-    return <h2>{error}</h2>;
-  }
-
-  return (
-    <>
-      <h2>Super Heroes Page</h2>
-      {data.map((hero) => {
-        return <div key={hero.name}>{hero.name}</div>;
-      })}
-    </>
-  );
+  return <div>RQ super heroes page</div>;
 };
 
 export default RQSuperHeroPage;
